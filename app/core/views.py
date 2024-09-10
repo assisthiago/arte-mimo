708c7b17902f1bc697af3fc606941321bdc41e53
Thiago Assis <thiago.medassis@gmail.com>
2024-09-10 18:24:47 -0300

    Finalizando 1

--- app/core/views.py
+++ app/core/views.py
@@ -4,13 +4,13 @@ from django.shortcuts import render
 def index(request):
     products = [
         {
-            "img": "https://images.unsplash.com/photo-1617519478819-9f578a5df62f?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fHw%3D",
+            "img": "https://images.unsplash.com/photo-1552899167-d0b2887ca549?q=80&w=2815&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             "type": "boneco",
             "preheading": "Boneco",
             "name": "Curology Campaign",
         },
         {
-            "img": "https://images.unsplash.com/photo-1617519478819-9f578a5df62f?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fHw%3D",
+            "img": "https://images.unsplash.com/photo-1552899167-d0b2887ca549?q=80&w=2815&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             "type": "boneco",
             "preheading": "Boneco",
             "name": "Curology Campaign",
@@ -22,7 +22,7 @@ def index(request):
             "name": "Honest Packaging",
         },
         {
-            "img": "https://images.unsplash.com/photo-1617519478819-9f578a5df62f?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fHw%3D",
+            "img": "https://images.unsplash.com/photo-1552899167-d0b2887ca549?q=80&w=2815&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             "type": "boneco",
             "preheading": "Boneco",
             "name": "Larq Bright Campaign",
@@ -34,37 +34,41 @@ def index(request):
             "name": "Doodle Pads",
         },
         {
-            "img": "https://images.unsplash.com/photo-1617519478819-9f578a5df62f?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDd8fHxlbnwwfHx8fHw%3D",
+            "img": "https://images.unsplash.com/photo-1552899167-d0b2887ca549?q=80&w=2815&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             "type": "boneco",
             "preheading": "Boneco",
             "name": "Larq Earthtones Collection",
         },
         {
-            "img": "https://images.unsplash.com/photo-1609580006779-13720a773a30?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "img": "https://images.unsplash.com/photo-1623936180584-7a422cd8f7fb?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             "type": "outros",
             "preheading": "Outros",
             "name": "Goby Brush Prototype",
         },
         {
-            "img": "https://images.unsplash.com/photo-1609580006779-13720a773a30?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "img": "https://images.unsplash.com/photo-1623936180584-7a422cd8f7fb?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             "type": "outros",
             "preheading": "Outros",
             "name": "Larq Bottle Redesign",
         },
         {
-            "img": "https://images.unsplash.com/photo-1609580006779-13720a773a30?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            "img": "https://images.unsplash.com/photo-1623936180584-7a422cd8f7fb?q=80&w=2832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             "type": "outros",
             "preheading": "Outros",
             "name": "Goby Packaging ",
         },
     ]
 
+    background = "https://plus.unsplash.com/premium_photo-1681210072901-edf04c718727?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+
     filters = [
         {"name": "Boneco", "slug": "boneco"},
         {"name": "Pano", "slug": "pano"},
         {"name": "Decoração", "slug": "decoracao"},
-        {"name": "Outros"},
+        {"name": "Outros", "slug": "outros"},
     ]
     return render(
-        request, "index.html", context={"products": products, "filters": filters}
+        request,
+        "index.html",
+        context={"products": products, "filters": filters, "background": background},
     )
